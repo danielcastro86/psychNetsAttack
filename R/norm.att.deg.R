ded3b36940094e04d254af4e569116cd1bb06f25
Daniel Castro <dcastro@ismai.pt>
2020-10-19 17:18:58 +0100

    Update norm.att.deg.R

--- R/norm.att.deg.R
+++ R/norm.att.deg.R
@@ -26,7 +26,7 @@ norm.att.deg <- function(g){
   radius<- integer(n-1)
   density<- integer(n-1)
   centralization<-integer(n-1)
-  diameter <- integer(n-1)
+  #diameter <- integer(n-1)
   components <- integer(n-1)
 
 
@@ -45,7 +45,7 @@ norm.att.deg <- function(g){
     radius[i]<-igraph::radius(g2)
     density[i]<- igraph::graph.density(g2)
     centralization[i]<-igraph::centr_degree(g2)$centralization
-    diameter[i] <- igraph::diameter(g2)
+    #diameter[i] <- igraph::diameter(g2)
     components[i] <- igraph::components(g2)$no
 
     g2 <- igraph::delete.vertices(g2, v=which(V(g2)$names==matri[i,1])) #remover vértice pela ordem na tabela
@@ -55,7 +55,7 @@ norm.att.deg <- function(g){
          vertex.label=labels, vertex.label.cex=0.8, edge.width=2)
   }}, movie.name = "graph.norm.att.deg.gif")
   #tabela com os resultados do ataque
-  df<-hellno::as.data.frame(cbind(matri, c(numberofvertices, NA), c(clustersizes, NA), c(cohesion, NA), c(averagepath, NA), c(adhesion, NA), c(edgedensity, NA), c(transitivity, NA), c(radius, NA), c(density, NA), c(centralization, NA), c(diameter, NA), c(components, NA)), stringAsFactors=FALSE)
-  names(df)<-c( "norm.att.deg.vertex", "norm.att.deg.value", "norm.att.deg.number.of.vertices", "norm.att.deg.maxcsize", "norm.att.deg.cohesion", "norm.att.deg.averagepath", "norm.att.deg.adhesion", "norm.att.deg.edgedensity", "norm.att.deg.transitivity", "norm.att.deg.radius", "norm.att.deg.density", "norm.att.deg.centralization", "norm.att.deg.diameter", "norm.att.deg.components")
+  df<-hellno::as.data.frame(cbind(matri, c(numberofvertices, NA), c(clustersizes, NA), c(cohesion, NA), c(averagepath, NA), c(adhesion, NA), c(edgedensity, NA), c(transitivity, NA), c(radius, NA), c(density, NA), c(centralization, NA), c(components, NA)), stringAsFactors=FALSE)
+  names(df)<-c( "norm.att.deg.vertex", "norm.att.deg.value", "norm.att.deg.number.of.vertices", "norm.att.deg.maxcsize", "norm.att.deg.cohesion", "norm.att.deg.averagepath", "norm.att.deg.adhesion", "norm.att.deg.edgedensity", "norm.att.deg.transitivity", "norm.att.deg.radius", "norm.att.deg.density", "norm.att.deg.centralization", "norm.att.deg.components")
   return(df)
 }
