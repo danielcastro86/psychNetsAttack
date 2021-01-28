bd44544a6564494b69b589ff22bf10c5382f544c
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2021-01-28 09:49:46 +0000

    nSampleNet update
    BGGM for ordinal variables added

--- R/nSampleNet.R
+++ R/nSampleNet.R
@@ -1,22 +1,23 @@
 
 
-nSampleNet <- function(nsamples, samplesize, data){
-  
+nSampleNet <- function(nsamples, samplesize, data, method){
+
   n <- nsamples
-  t <- type
-  
- 
+
+ if(method =="ggmModSelect"){
+
+
   global <- list()
-  
+
   for(i in 1:n){
-    
+
     gRand <- estimateNetwork(sample_n(data, samplesize), default = "ggmModSelect")
     qRand <- qgraph::qgraph(gRand$graph, layout="spring", directed= FALSE, theme = "TeamFortress", DoNotPlot=T)
     iRand <- as.igraph(qRand, attributes=TRUE)
-    
+
     global[[i]] <- netDesc(iRand)
   }
-  
+
   nodes <- integer()
   edges <- integer()
   negEdges <- integer()
@@ -27,9 +28,9 @@ nSampleNet <- function(nsamples, samplesize, data){
   compact <- integer()
   globalClus <- integer()
   globalTransit <- integer()
-  
-  for (i in 1:5) {
-    
+
+  for (i in 1:n) {
+
   nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
   edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
   negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
@@ -40,11 +41,11 @@ nSampleNet <- function(nsamples, samplesize, data){
   compact[i] <- global[[i]][["Global Properties"]][["Compactness"]]
   globalClus[i] <- global[[i]][["Global Properties"]][["Global.Clustering.Coefficient"]]
   globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
-  
-    
-    
+
+
+
   }
-  
+
   mn <- mean(nodes)
   me <- mean(edges)
   mne <- mean(negEdges)
@@ -55,20 +56,84 @@ nSampleNet <- function(nsamples, samplesize, data){
   mdens <- mean(dens)
   mglobalClus <- mean(globalClus)
   mglobalTransit <- mean(globalTransit)
-  
+
   df <- data.frame(mn, me, mne, miN, mcomp, mcompact, mavePath, mdens, mglobalClus, mglobalTransit)
   names(df) <- c("nodes", "edges", "negative edges", "isolated nodes", "components",
                  "compactness", "average path lenght", "density", "global cluster.",
                  "global transit.")
-  
+
   res.df <- list("Mean Values" = c(df), "Complete Results" = c(global))
-  
+
   return(res.df)
-  
+
+ }
+
+  if(method =="BGGM1"){
+
+
+    global <- list()
+
+    for(i in 1:n){
+
+      gRand <- BGGM::estimate(sample_n(data, samplesize), type = "ordinal")
+      gRand <- BGGM::select(gRand)
+      qRand <- qgraph::qgraph(gRand$pcor_adj, layout="spring", directed= FALSE, theme = "TeamFortress", DoNotPlot=T)
+      iRand <- as.igraph(qRand, attributes=TRUE)
+
+      global[[i]] <- netDesc(iRand)
+    }
+
+    nodes <- integer()
+    edges <- integer()
+    negEdges <- integer()
+    isoNodes <- integer()
+    comp <- integer()
+    avePath <- integer()
+    dens <- integer()
+    compact <- integer()
+    globalClus <- integer()
+    globalTransit <- integer()
+
+    for (i in 1:n) {
+
+      nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
+      edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
+      negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
+      isoNodes[i] <- global[[i]][["Global Properties"]][["Isolated.Nodes"]]
+      comp[i] <- global[[i]][["Global Properties"]][["Components"]]
+      avePath[i] <- global[[i]][["Global Properties"]][["Ave.Path"]]
+      dens[i] <- global[[i]][["Global Properties"]][["Density"]]
+      compact[i] <- global[[i]][["Global Properties"]][["Compactness"]]
+      globalClus[i] <- global[[i]][["Global Properties"]][["Global.Clustering.Coefficient"]]
+      globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
+
+
+
+    }
+
+    mn <- mean(nodes)
+    me <- mean(edges)
+    mne <- mean(negEdges)
+    miN <- mean(isoNodes)
+    mcomp <- mean(comp)
+    mcompact <- mean(compact)
+    mavePath <- mean(avePath)
+    mdens <- mean(dens)
+    mglobalClus <- mean(globalClus)
+    mglobalTransit <- mean(globalTransit)
+
+    df <- data.frame(mn, me, mne, miN, mcomp, mcompact, mavePath, mdens, mglobalClus, mglobalTransit)
+    names(df) <- c("nodes", "edges", "negative edges", "isolated nodes", "components",
+                   "compactness", "average path lenght", "density", "global cluster.",
+                   "global transit.")
+
+    res.df <- list("Mean Values" = c(df), "Complete Results" = c(global))
+
+    return(res.df)
+
+  }
 }
 
 
- 
- 
- 
- 
+
+
