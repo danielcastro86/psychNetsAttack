3f2cb8bad2fad58f6b043d2af63f71dda4e27150
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2021-03-10 11:37:55 +0000

    added snakecase package

--- R/plot.att.R
+++ R/plot.att.R
@@ -77,6 +77,7 @@ plot.att <- function(attackscores){
       scale_x_continuous(labels = function(x) paste0(x * 100, '%'), breaks = c(0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1)) + theme_gray()
 
 
-    return(grid.arrange(AvePathPlot, DensityPlot, ComponentsPlot, nrow=3, ncol=1, top=paste0(names(scores)[[1]])))
+    return(grid.arrange(AvePathPlot, DensityPlot, ComponentsPlot, nrow=3, ncol=1, top=paste0(to_any_case(names(scores[[1]]), case = "title"))))
 
 }
+
