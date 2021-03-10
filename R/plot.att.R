b3c0a7fc1d0c4d03ddc9f09d5cfd12d964b26b75
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2021-03-10 12:20:21 +0000

    plot.att updates

--- R/plot.att.R
+++ R/plot.att.R
@@ -16,7 +16,7 @@ plot.att <- function(attackscores){
     graphdata <- scores[[1]] %>% dplyr::select(14,6)
     graphdata <- cbind(graphdata, scores[[2]][[6]])
 
-    colnames(graphdata) <- c("Proportion of Nodes Removed", "Attack Score", "Random Attack Score")
+    colnames(graphdata) <- c("Proportion of Nodes Removed", paste0(to_any_case(names(scores)[[1]], case = "title")), "Random Attack Scores")
 
     graphdata <- melt(graphdata, id.vars = "Proportion of Nodes Removed")
     graphdata$value <- as.numeric(as.character(graphdata$value))
@@ -28,7 +28,7 @@ plot.att <- function(attackscores){
                                          y= `Average Path Length`,
                                          group= variable,
                                          color=variable)) +
-
+      labs(color="Attack Type") +
       geom_line(linetype = "dotted") +
       geom_point(shape = 19, size = 2) +
       scale_y_continuous() +
@@ -38,7 +38,7 @@ plot.att <- function(attackscores){
     graphdata <- scores[[1]] %>% dplyr::select(14,11)
     graphdata <- cbind(graphdata, scores[[2]][[11]])
 
-    colnames(graphdata) <- c("Proportion of Nodes Removed", "Attack Score", "Random Attack Score")
+    colnames(graphdata) <- c("Proportion of Nodes Removed", paste0(to_any_case(names(scores)[[1]], case = "title")), "Random Attack Scores")
 
     graphdata <- melt(graphdata, id.vars = "Proportion of Nodes Removed")
     graphdata$value <- as.numeric(as.character(graphdata$value))
@@ -49,7 +49,7 @@ plot.att <- function(attackscores){
                                          y= `Network Density`,
                                          group= variable,
                                          color=variable)) +
-
+      labs(color="Attack Type") +
       geom_line(linetype = "dotted") +
       geom_point(shape = 19, size = 2) +
       scale_y_continuous() +
@@ -59,7 +59,7 @@ plot.att <- function(attackscores){
     graphdata <- scores[[1]] %>% dplyr::select(14,13)
     graphdata <- cbind(graphdata, scores[[2]][[13]])
 
-    colnames(graphdata) <- c("Proportion of Nodes Removed", "Attack Score", "Random Attack Score")
+    colnames(graphdata) <- c("Proportion of Nodes Removed", paste0(to_any_case(names(scores)[[1]], case = "title")), "Random Attack Scores")
 
     graphdata <- melt(graphdata, id.vars = "Proportion of Nodes Removed")
     graphdata$value <- as.numeric(as.character(graphdata$value))
@@ -70,11 +70,12 @@ plot.att <- function(attackscores){
                                             y= `Number of Components`,
                                             group= variable,
                                             color=variable)) +
-
+      labs(color="Attack Type") +
       geom_line(linetype = "dotted") +
       geom_point(shape = 19, size = 2) +
       scale_y_continuous() +
       scale_x_continuous(labels = function(x) paste0(x * 100, '%'), breaks = c(0,0.05,0.1,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5,0.55,0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,1)) + theme_gray()
+
 
 
     return(grid.arrange(AvePathPlot, DensityPlot, ComponentsPlot, nrow=3, ncol=1, top=paste0(to_any_case(names(scores)[[1]], case = "title"))))
