56a2b0eb56954c37ca397e58fc76171fef4cf9c5
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2025-01-16 19:00:32 +0000

    netDesc changes
    Change in netDesc functions to remove QuACN package related metrics.

--- R/netDesc.R
+++ R/netDesc.R
@@ -1,71 +1,65 @@
-#netDescriptives
-
-netDesc <- function(graph){
+netDesc <- function(graph) {
 
   g <- graph
 
-  #igraph
-  nv <- gorder(graph = g)   #number of vertices
-  ne <- gsize(graph =g)    #number of edg
-  iso <- sum(igraph::degree(g)==0) #isolated
+  # igraph
+  nv <- gorder(graph = g)   # number of vertices
+  ne <- gsize(graph = g)    # number of edges
+  iso <- sum(igraph::degree(g) == 0) # isolated nodes
   g.nComponents <- clusters(g)
-  nComp <- g.nComponents$no #number of components
-  av.path.lenght <- average.path.length(graph = g, unconnected = TRUE)
+  nComp <- g.nComponents$no # number of components
+  absgraph <- g
+  E(absgraph)$weight <- abs(E(absgraph)$weight)
+  av.path.lenght <- average.path.length(graph = absgraph, unconnected = TRUE)
   density <- edge_density(graph = g, loops = FALSE)
   connectivity <- sum(abs(E(g)$weight))
-  negEdges <- length(E(g)[which(E(g)$weight<0)])
+  negEdges <- length(E(g)[which(E(g)$weight < 0)])
 
-  #pulsar
+  # pulsar
   estrada <- pulsar::estrada.class(as_adjacency_matrix(g, attr = "weight", type = "both"))
 
-  #QuACN
-  compactness <- QuACN::compactness(as_graphnel(g))
-  globalClusterCoef <- QuACN::globalClusteringCoeff(as_graphnel(g))
-  localClusterCoef <- QuACN::localClusteringCoeff(as_graphnel(g))
-  gDistanceCompl <- QuACN::graphDistanceComplexity(as_graphnel(g))
-  gIndexCompl <- QuACN::graphIndexComplexity(as_graphnel(g))
-  gVertexCompl <- QuACN::graphVertexComplexity(as_graphnel(g))
-  gindexBcompl <- QuACN::complexityIndexB(as_graphnel(g))
-
-  #motifs (beta)
+  # motifs (beta)
   motif <- motifs(g)
 
-  #average degree
-  avdeg <- gsize(graph =g) / gorder(graph = g)
+  # average degree
+  avdeg <- gsize(graph = g) / gorder(graph = g)
 
-  #transitivity
+  # transitivity
   globalTransit <- igraph::transitivity(g, type = "global", isolates = "zero")
-
   localTransit <- igraph::transitivity(g, type = "local", isolates = "zero")
 
+  # Data frames
+  netDesc1 <- hellno::data.frame(
+    "Nodes" = nv,
+    "Edges" = ne,
+    "Negative Edges" = negEdges,
+    "Isolated Nodes" = iso,
+    "Components" = nComp,
+    "Ave.Path" = av.path.lenght,
+    "Density" = density,
+    "Global Transitivity" = globalTransit,
+    "Connectivity" = connectivity,
+    "Average Degree" = avdeg
 
+  )
 
+  netDesc2 <- hellno::data.frame(
+    "Estrada Class" = estrada
+)
 
-  netDesc1 <- (hellno::data.frame("Nodes"= nv, "Edges" = ne,"Negative Edges" = negEdges,
-                                  "Isolated Nodes" =  iso, "Components" = nComp,
-                                  "Ave.Path" =  av.path.lenght, "Density" = density,
-                                  "Compactness" = compactness, "Global Clustering Coefficient"= globalClusterCoef,
-                                  "Global Transitivity" = globalTransit, "Connectivity" = connectivity))
+  netDesc3 <- hellno::data.frame(
+    "Local Transitivity" = localTransit
+  )
 
-  netDesc2 <- hellno::data.frame("Estrada Class" = estrada, "Average Degree" = avdeg)
-
-  netDesc3 <- hellno::data.frame("Local Clustering Coefficient" = localClusterCoef,
-                                 "Local Transitivity" = localTransit)
-
-  netDesc4 <- hellno::data.frame(hellno::data.frame("Distance" = gDistanceCompl,
-                                                    "Vertex" = gVertexCompl,
-                                                    "Index" = gIndexCompl,
-                                                    "Index B" = gindexBcompl))
   netDesc5 <- hellno::data.frame(motif)
 
-  netDesc <- list("Global Properties" = c(netDesc1), "Network Class" = c(netDesc2),
-                  "Local Properties" = c(netDesc3), "Complexity Measures" = c(netDesc4),
-                  "Motif" = c(netDesc5))
-
+  # Compile results into a list
+  netDesc <- list(
+    "Global Properties" = c(netDesc1),
+    "Network Class" = c(netDesc2),
+    "Local Properties" = c(netDesc3),
+    "Motif" = c(netDesc5)
+  )
 
   return(netDesc)
-
-
 }
-
-
