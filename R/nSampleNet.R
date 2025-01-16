b032b949d250c88257b5caf3fa9e6349edab6dbe
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2025-01-16 19:22:13 +0000

    nSampleNet
    Changes in nSampleNet to remove quacn metrics and add CI calculation as well as improve code readability

--- R/nSampleNet.R
+++ R/nSampleNet.R
@@ -1,139 +1,79 @@
-
-
 nSampleNet <- function(nsamples, samplesize, data, method){
 
   n <- nsamples
 
- if(method =="ggmModSelect"){
-
-
   global <- list()
 
   for(i in 1:n){
+    if(method == "ggmModSelect"){
+      gRand <- estimateNetwork(sample_n(data, samplesize), default = "ggmModSelect")
+    } else if(method == "BGGM1"){
+      gRand <- BGGM::estimate(sample_n(data, samplesize), type = "ordinal")
+      gRand <- BGGM::select(gRand)
+    } else {
+      stop("Method not recognized. Use 'ggmModSelect' or 'BGGM1'.")
+    }
 
-    gRand <- estimateNetwork(sample_n(data, samplesize), default = "ggmModSelect")
-    qRand <- qgraph::qgraph(gRand$graph, layout="spring", directed= FALSE, theme = "TeamFortress", DoNotPlot=T)
+    qRand <- qgraph::qgraph(gRand$graph, layout="spring", directed=FALSE, theme="TeamFortress", DoNotPlot=T)
     iRand <- as.igraph(qRand, attributes=TRUE)
 
     global[[i]] <- netDesc(iRand)
   }
 
-  nodes <- integer()
-  edges <- integer()
-  negEdges <- integer()
-  isoNodes <- integer()
-  comp <- integer()
-  avePath <- integer()
-  dens <- integer()
-  compact <- integer()
-  globalClus <- integer()
-  globalTransit <- integer()
+  # Inicializa as variáveis de propriedades
+  nodes <- integer(n)
+  edges <- integer(n)
+  negEdges <- integer(n)
+  isoNodes <- integer(n)
+  comp <- integer(n)
+  avePath <- numeric(n)
+  dens <- numeric(n)
+  globalTransit <- numeric(n)
 
+  # Preenche as variáveis com os valores extraídos
   for (i in 1:n) {
-
-  nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
-  edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
-  negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
-  isoNodes[i] <- global[[i]][["Global Properties"]][["Isolated.Nodes"]]
-  comp[i] <- global[[i]][["Global Properties"]][["Components"]]
-  avePath[i] <- global[[i]][["Global Properties"]][["Ave.Path"]]
-  dens[i] <- global[[i]][["Global Properties"]][["Density"]]
-  compact[i] <- global[[i]][["Global Properties"]][["Compactness"]]
-  globalClus[i] <- global[[i]][["Global Properties"]][["Global.Clustering.Coefficient"]]
-  globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
-
-
-
+    nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
+    edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
+    negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
+    isoNodes[i] <- global[[i]][["Global Properties"]][["Isolated.Nodes"]]
+    comp[i] <- global[[i]][["Global Properties"]][["Components"]]
+    avePath[i] <- global[[i]][["Global Properties"]][["Ave.Path"]]
+    dens[i] <- global[[i]][["Global Properties"]][["Density"]]
+    globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
   }
 
-  mn <- mean(nodes)
-  me <- mean(edges)
-  mne <- mean(negEdges)
-  miN <- mean(isoNodes)
-  mcomp <- mean(comp)
-  mcompact <- mean(compact)
-  mavePath <- mean(avePath)
-  mdens <- mean(dens)
-  mglobalClus <- mean(globalClus)
-  mglobalTransit <- mean(globalTransit)
+  # Função para calcular intervalos de confiança
+  calculate_ci <- function(x) {
+    se <- sd(x) / sqrt(length(x))  # Erro padrão
+    mean_x <- mean(x)
+    lower <- mean_x - 1.96 * se
+    upper <- mean_x + 1.96 * se
+    return(c(mean = mean_x, lower = lower, upper = upper))
+  }
 
-  df <- data.frame(mn, me, mne, miN, mcomp, mcompact, mavePath, mdens, mglobalClus, mglobalTransit)
-  names(df) <- c("nodes", "edges", "negative edges", "isolated nodes", "components",
-                 "compactness", "average path lenght", "density", "global cluster.",
-                 "global transit.")
+  # Cálculo de médias e IC para cada métrica
+  nodes_ci <- calculate_ci(nodes)
+  edges_ci <- calculate_ci(edges)
+  negEdges_ci <- calculate_ci(negEdges)
+  isoNodes_ci <- calculate_ci(isoNodes)
+  comp_ci <- calculate_ci(comp)
+  avePath_ci <- calculate_ci(avePath)
+  dens_ci <- calculate_ci(dens)
+  globalTransit_ci <- calculate_ci(globalTransit)
 
-  res.df <- list("Mean Values" = c(df), "Complete Results" = c(global))
+  # Criação do data frame de resultados
+  df <- data.frame(
+    Metric = c("nodes", "edges", "negative edges", "isolated nodes", "components",
+               "average path length", "density", "global transitivity"),
+    Mean = c(nodes_ci["mean"], edges_ci["mean"], negEdges_ci["mean"], isoNodes_ci["mean"],
+             comp_ci["mean"], avePath_ci["mean"], dens_ci["mean"], globalTransit_ci["mean"]),
+    Lower_CI = c(nodes_ci["lower"], edges_ci["lower"], negEdges_ci["lower"], isoNodes_ci["lower"],
+                 comp_ci["lower"], avePath_ci["lower"], dens_ci["lower"], globalTransit_ci["lower"]),
+    Upper_CI = c(nodes_ci["upper"], edges_ci["upper"], negEdges_ci["upper"], isoNodes_ci["upper"],
+                 comp_ci["upper"], avePath_ci["upper"], dens_ci["upper"], globalTransit_ci["upper"])
+  )
+
+  res.df <- list("Mean Values and CI" = df, "Complete Results" = global)
 
   return(res.df)
-
- }
-
-  if(method =="BGGM1"){
-
-
-    global <- list()
-
-    for(i in 1:n){
-
-      gRand <- BGGM::estimate(sample_n(data, samplesize), type = "ordinal")
-      gRand <- BGGM::select(gRand)
-      qRand <- qgraph::qgraph(gRand$pcor_adj, layout="spring", directed= FALSE, theme = "TeamFortress", DoNotPlot=T)
-      iRand <- as.igraph(qRand, attributes=TRUE)
-
-      global[[i]] <- netDesc(iRand)
-    }
-
-    nodes <- integer()
-    edges <- integer()
-    negEdges <- integer()
-    isoNodes <- integer()
-    comp <- integer()
-    avePath <- integer()
-    dens <- integer()
-    compact <- integer()
-    globalClus <- integer()
-    globalTransit <- integer()
-
-    for (i in 1:n) {
-
-      nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
-      edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
-      negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
-      isoNodes[i] <- global[[i]][["Global Properties"]][["Isolated.Nodes"]]
-      comp[i] <- global[[i]][["Global Properties"]][["Components"]]
-      avePath[i] <- global[[i]][["Global Properties"]][["Ave.Path"]]
-      dens[i] <- global[[i]][["Global Properties"]][["Density"]]
-      compact[i] <- global[[i]][["Global Properties"]][["Compactness"]]
-      globalClus[i] <- global[[i]][["Global Properties"]][["Global.Clustering.Coefficient"]]
-      globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
-
-
-
-    }
-
-    mn <- mean(nodes)
-    me <- mean(edges)
-    mne <- mean(negEdges)
-    miN <- mean(isoNodes)
-    mcomp <- mean(comp)
-    mcompact <- mean(compact)
-    mavePath <- mean(avePath)
-    mdens <- mean(dens)
-    mglobalClus <- mean(globalClus)
-    mglobalTransit <- mean(globalTransit)
-
-    df <- data.frame(mn, me, mne, miN, mcomp, mcompact, mavePath, mdens, mglobalClus, mglobalTransit)
-    names(df) <- c("nodes", "edges", "negative edges", "isolated nodes", "components",
-                   "compactness", "average path lenght", "density", "global cluster.",
-                   "global transit.")
-
-    res.df <- list("Mean Values" = c(df), "Complete Results" = c(global))
-
-    return(res.df)
-
-  }
 }
-
-
-
-
