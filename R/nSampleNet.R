ca8031c9067ffca97fcc19b96bd4451dd97cd233
danielcastro86 <72454041+danielcastro86@users.noreply.github.com>
2025-01-17 15:51:47 +0000

    nSampleNet corrected
    correction to function and increased readability

--- R/nSampleNet.R
+++ R/nSampleNet.R
@@ -1,79 +1,65 @@
-nSampleNet <- function(nsamples, samplesize, data, method){
-
-  n <- nsamples
-
-  global <- list()
-
-  for(i in 1:n){
-    if(method == "ggmModSelect"){
-      gRand <- estimateNetwork(sample_n(data, samplesize), default = "ggmModSelect")
-    } else if(method == "BGGM1"){
-      gRand <- BGGM::estimate(sample_n(data, samplesize), type = "ordinal")
-      gRand <- BGGM::select(gRand)
-    } else {
-      stop("Method not recognized. Use 'ggmModSelect' or 'BGGM1'.")
-    }
-
-    qRand <- qgraph::qgraph(gRand$graph, layout="spring", directed=FALSE, theme="TeamFortress", DoNotPlot=T)
-    iRand <- as.igraph(qRand, attributes=TRUE)
-
-    global[[i]] <- netDesc(iRand)
-  }
-
-  # Inicializa as variáveis de propriedades
-  nodes <- integer(n)
-  edges <- integer(n)
-  negEdges <- integer(n)
-  isoNodes <- integer(n)
-  comp <- integer(n)
-  avePath <- numeric(n)
-  dens <- numeric(n)
-  globalTransit <- numeric(n)
-
-  # Preenche as variáveis com os valores extraídos
-  for (i in 1:n) {
-    nodes[i] <- global[[i]][["Global Properties"]][["Nodes"]]
-    edges[i] <- global[[i]][["Global Properties"]][["Edges"]]
-    negEdges[i] <- global[[i]][["Global Properties"]][["Negative.Edges"]]
-    isoNodes[i] <- global[[i]][["Global Properties"]][["Isolated.Nodes"]]
-    comp[i] <- global[[i]][["Global Properties"]][["Components"]]
-    avePath[i] <- global[[i]][["Global Properties"]][["Ave.Path"]]
-    dens[i] <- global[[i]][["Global Properties"]][["Density"]]
-    globalTransit[i] <- global[[i]][["Global Properties"]][["Global.Transitivity"]]
-  }
-
-  # Função para calcular intervalos de confiança
+nSampleNet <- function(nsamples, samplesize, data, method) {
   calculate_ci <- function(x) {
-    se <- sd(x) / sqrt(length(x))  # Erro padrão
+    se <- sd(x) / sqrt(length(x))  # Standard Error
     mean_x <- mean(x)
     lower <- mean_x - 1.96 * se
     upper <- mean_x + 1.96 * se
-    return(c(mean = mean_x, lower = lower, upper = upper))
+    c(mean = mean_x, lower = lower, upper = upper)
   }
 
-  # Cálculo de médias e IC para cada métrica
-  nodes_ci <- calculate_ci(nodes)
-  edges_ci <- calculate_ci(edges)
-  negEdges_ci <- calculate_ci(negEdges)
-  isoNodes_ci <- calculate_ci(isoNodes)
-  comp_ci <- calculate_ci(comp)
-  avePath_ci <- calculate_ci(avePath)
-  dens_ci <- calculate_ci(dens)
-  globalTransit_ci <- calculate_ci(globalTransit)
+  get_global_properties <- function(global_results, metrics) {
+    result_list <- lapply(metrics, function(metric) {
+      sapply(global_results, function(res) res[["Global Properties"]][[metric]])
+    })
+    names(result_list) <- metrics
+    result_list
+  }
 
-  # Criação do data frame de resultados
-  df <- data.frame(
-    Metric = c("nodes", "edges", "negative edges", "isolated nodes", "components",
-               "average path length", "density", "global transitivity"),
-    Mean = c(nodes_ci["mean"], edges_ci["mean"], negEdges_ci["mean"], isoNodes_ci["mean"],
-             comp_ci["mean"], avePath_ci["mean"], dens_ci["mean"], globalTransit_ci["mean"]),
-    Lower_CI = c(nodes_ci["lower"], edges_ci["lower"], negEdges_ci["lower"], isoNodes_ci["lower"],
-                 comp_ci["lower"], avePath_ci["lower"], dens_ci["lower"], globalTransit_ci["lower"]),
-    Upper_CI = c(nodes_ci["upper"], edges_ci["upper"], negEdges_ci["upper"], isoNodes_ci["upper"],
-                 comp_ci["upper"], avePath_ci["upper"], dens_ci["upper"], globalTransit_ci["upper"])
-  )
+  calculate_metrics <- function(global_properties) {
+    lapply(global_properties, calculate_ci)
+  }
 
-  res.df <- list("Mean Values and CI" = df, "Complete Results" = global)
+  summarize_results <- function(ci_results, metric_labels) {
+    data.frame(
+      Metric = metric_labels,
+      Mean = sapply(ci_results, `[[`, "mean"),
+      Lower_CI = sapply(ci_results, `[[`, "lower"),
+      Upper_CI = sapply(ci_results, `[[`, "upper")
+    )
+  }
 
-  return(res.df)
+  if (!method %in% c("ggmModSelect", "BGGM1")) {
+    stop("Invalid method. Choose either 'ggmModSelect' or 'BGGM1'.")
+  }
+
+  global_results <- vector("list", nsamples)
+
+  for (i in seq_len(nsamples)) {
+    sampled_data <- dplyr::sample_n(data, samplesize, replace = (method == "BGGM1"))
+
+    if (method == "ggmModSelect") {
+      gRand <- estimateNetwork(sampled_data, default = "ggmModSelect")
+      graph <- gRand$graph
+    } else if (method == "BGGM1") {
+      gRand <- BGGM::estimate(sampled_data, type = "mixed")
+      gRand <- BGGM::select(gRand)
+      graph <- gRand$pcor_adj
+    }
+
+    qRand <- qgraph::qgraph(graph, layout = "spring", directed = FALSE, theme = "TeamFortress", DoNotPlot = TRUE)
+    iRand <- igraph::as.igraph(qRand, attributes = TRUE)
+    global_results[[i]] <- netDesc(iRand)
+  }
+
+  metrics <- c("Nodes", "Edges", "Negative.Edges", "Isolated.Nodes", "Components",
+               "Ave.Path", "Density", "Global.Transitivity", "Connectivity", "Average.Degree")
+
+  metric_labels <- c("nodes", "edges", "negative edges", "isolated nodes", "components",
+                     "average path length", "density", "global transitivity", "connectivity", "average degree")
+
+  global_properties <- get_global_properties(global_results, metrics)
+  ci_results <- calculate_metrics(global_properties)
+  summary_df <- summarize_results(ci_results, metric_labels)
+
+  list("Mean Values and CI" = summary_df, "Complete Results" = global_results)
 }
